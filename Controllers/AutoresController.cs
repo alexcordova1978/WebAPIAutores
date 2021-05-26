4108a6e85730110e06cfbafbaaf35e088b18f9a7
Alex Córdova <walex.cordova@outlook.com>
2021-05-26 09:30:55 -0600

    Add project files.

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() {Id=1 , Nombre= "Alex" },
+                new Autor() {Id=2 , Nombre="Zulma" }
+            };
+        }
+    }
+}
